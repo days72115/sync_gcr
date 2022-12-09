74ea2d840405364d3d194664c4312621dc34e8e5
days72115 <jianmingxu@gmail.com>
2022-12-09 21:34:28 +0800

    Update load_image.py

--- load_image.py
+++ load_image.py
@@ -11,7 +11,15 @@ def get_filename():
 def pull_image():
     name_list= get_filename()
     for name in name_list:
-        new_name = "kenwood/" + name.split("/")[-1]
+        if 'sha256' in name:
+            print(name)
+            sha256_name = name.split("@")
+            new_name_t = sha256_name[0].split("/")[-1]
+            tag = sha256_name[-1].split(":")[-1][0:6]
+            new_name = "kenwood/" + new_name_t + ":"+ tag
+        else:
+            new_name = "kenwood/" + name.split("/")[-1]
+        print(new_name)
         subprocess.call("docker pull {}".format(new_name), shell=True)
         subprocess.run(["docker", "tag", new_name, name])
         subprocess.call("docker rmi  {}".format(new_name), shell= True)
